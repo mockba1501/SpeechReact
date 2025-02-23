5d5e19b7f4c4c595de44b9ad78a8a469907ff8af
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-02-23 20:52:57 +0200

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
+const cors = require('cors');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3001;
+console.log('PORT:', port);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+app.use(cors());
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get('/api/get-speech-token', async (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    const speechKey = process.env.SPEECH_KEY;
+    const speechRegion = process.env.SPEECH_REGION;
+    
+    if (speechKey === 'paste-your-speech-key-here' || speechRegion === 'paste-your-speech-region-here') {
+        res.status(400).send('You forgot to add your speech key or region to the .env file.');
+    } 
+    else 
+    {
+        const headers = { 
+            headers: {
+                'Ocp-Apim-Subscription-Key': speechKey,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        };
+
+        try {
+            const tokenResponse = await axios.post(`https://${speechRegion}.api.cognitive.microsoft.com/sts/v1.0/issueToken`, null, headers);
+            //console.log('Token fetched from Azure:', tokenResponse.data); // Log the token
+            res.send({ token: tokenResponse.data, region: speechRegion });
+        } catch (err) {
+            res.status(401).send('There was an error authorizing your speech key.');
+        }
+    }
+});
+
+app.listen(port, () =>
+    console.log(`Express server is running on localhost:${port}`)
+);
