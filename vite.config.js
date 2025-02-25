5a49c2699ada1f278ab2ec57b551bb67de45defb
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-02-25 18:47:11 +0200

    Adjust vite configurations

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Build-specific configurations
+  build: {
+    sourcemap: true, // Enable source maps for debugging
+    minify: 'false', // Use Terser for minification (default)
+    /*terserOptions: {
+      compress: {
+        drop_console: true, // Remove console.log statements in production
+        drop_debugger: true, // Remove debugger statements in production
+      },
+    },
+    outDir: 'dist', // Output directory (default is 'dist')
+    emptyOutDir: true, // Empty the output directory before building*/
+  },
 })
