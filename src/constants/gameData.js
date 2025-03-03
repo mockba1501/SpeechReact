8056d9dfa80a58839ab9d3a77758cec7c3bd1638
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-03-03 14:54:20 +0200

    Reorder game settings
    Classic Hangman can be played directly
    Customized Hangman will go through the settings menu first

--- src/constants/gameData.js
+++ src/constants/gameData.js
@@ -1,22 +1,21 @@
 export const games = [
-  { id: "hangman", name: "Hangman", description: "Classic word guessing game." },
-  { id: "word-puzzle", name: "Word Puzzle", description: "Solve the missing letters." },
+  { id: "hangman", name: "Classic Hangman", description: "Classic word guessing game." },
   { id: "hangman2", name: "Hangman", description: "Classic word guessing game." },
+  { id: "word-puzzle", name: "Word Puzzle", description: "Solve the missing letters." },
   { id: "word-puzzle2", name: "Word Puzzle", description: "Solve the missing letters." },
   { id: "hangman3", name: "Hangman", description: "Classic word guessing game." },
   { id: "word-puzzle3", name: "Word Puzzle", description: "Solve the missing letters." }
 ];
 
 export const gameSettings = {
-    "hangman": [
-      { id: "age", label: "Age", options: ["5 to 7 years old", "8 to 13 years old", "14 to 17 years old", "more than 18 years old"] },
-      { id: "sound", label: "Letter Sound", options: ["/p/ - pat", "/b/ - bat","/t/ - top","/d/ - dog","/k/  cat",
-          "/g/ - go","/m/ - man","/n/ - net","/ŋ/ - sing","/f/ - fan","/v/ - van","/s/ - sun","/z/ - zoo","/ʃ/ - shoe","/ʧ/ - chair"] },
-      { id: "position", label: "Letter Position", options: ["Beginning","Middle","End"]}
-    ],
+    "hangman": [ ], // No settings available for Hangman
     "word-puzzle": [
       { id: "timeLimit", label: "Time Limit", options: ["30s", "60s", "90s"] },
       { id: "hints", label: "Hints Allowed", options: ["Yes", "No"] }
     ],
-    "hangman2": [ ] // If no settings are available
+    "hangman2": [ { id: "age", label: "Age", options: ["5 to 7 years old", "8 to 13 years old", "14 to 17 years old", "more than 18 years old"] },
+    { id: "sound", label: "Letter Sound", options: ["/p/ - pat", "/b/ - bat","/t/ - top","/d/ - dog","/k/  cat",
+        "/g/ - go","/m/ - man","/n/ - net","/ŋ/ - sing","/f/ - fan","/v/ - van","/s/ - sun","/z/ - zoo","/ʃ/ - shoe","/ʧ/ - chair"] },
+    { id: "position", label: "Letter Position", options: ["Beginning","Middle","End"]}
+    ]
   };
