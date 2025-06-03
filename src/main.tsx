a2981b565744fd39d7ca11077a8b287717ce8cc5
Mohammed Ockba <7790976+mockba1501@users.noreply.github.com>
2025-06-04 01:10:53 +0300

    Fixing routing and app entry

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App'
 import AppRoutes from './routes'
 
-createRoot(document.getElementById('root')).render(
+const rootElement = document.getElementById('root');
+if (!rootElement) throw new Error("Failed to find the root element");
+
+createRoot(rootElement).render(
   <StrictMode>
-    <AppRoutes>
-    <App />
-    </AppRoutes>
+   {/* <AppRoutes>
+      <App /> */}
+    <AppRoutes/>
   </StrictMode>,
 )
